88e5adc240133563e51c841e8c312757a3738813
Alex Sullon <alexsullonporras@hotmail.com>
2025-09-30 11:57:40 -0500

    Modificación del listado de datos en el repositorio PrestamoRepositorio

--- Financiera.Data/PrestamoRepositorio.cs
+++ Financiera.Data/PrestamoRepositorio.cs
@@ -20,25 +20,44 @@ namespace Financiera.Data
         public List<Prestamo> Listar()
         {
             List<Prestamo> listado = new List<Prestamo>();
-            listado.Add(new Prestamo
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
             {
-                ID = 1,
-                ClienteID = 1,
-                Estado = "P",
-                Fecha = DateTime.Today,
-                FechaDeposito= DateTime.Today.AddMonths(1),
-                Importe = 6500,
-                Moneda = "PEN",
-                Plazo = 6,
-                Tasa = 28.5M,
-                TipoPrestamoID = 2
-            });
+                conexion.Open();
+                using (SqlCommand comando = new SqlCommand("ListarPrestamos", conexion))
+                {
+                    comando.CommandType = System.Data.CommandType.StoredProcedure;
+                    using (SqlDataReader lector = comando.ExecuteReader())
+                    {
+                        if (lector != null && lector.HasRows)
+                        {
+                            while (lector.Read())
+                            {
+                                listado.Add(new Prestamo
+                                {
+                                    ID = lector.GetInt32(0),
+                                    Fecha = lector.GetDateTime(1),
+                                    FechaDeposito = lector.GetDateTime(2),
+                                    ClienteID = lector.GetInt32(3),
+                                    TipoPrestamoID = lector.GetInt32(4),
+                                    Moneda = lector.GetString(5),
+                                    Importe = lector.GetDecimal(6),
+                                    Plazo = lector.GetInt32(7),
+                                    Tasa = lector.GetDecimal(8),
+                                    Estado = lector.GetString(9)
+                                });
+                            }
+                        }
+                    }
+
+                }
+            }
             return listado;
         }
 
         public Prestamo ObtenerPorID(int id)
         {
             Prestamo prestamo = null;
+            
             return prestamo;
         }
 
